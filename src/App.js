1cef384597db340523ee39fd67f9ab82c761b3bb
Lina <g.korotkova@lucky-team.pro>
2024-11-19 17:06:59 +0300

    init react app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
+import Snowflake from "./Components/Snowflake";
+import ColorFix from "./Components/Color";
 
 function App() {
+  const [color, setColor] = useState('');
+  const [rgbColor, setRgbColor] = useState('rgb(x, x, x)');
+
+  console.log(color)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Snowflake rgbColor={rgbColor} />
+      <ColorFix color={color} setColor={setColor} rgbColor={rgbColor} setRgbColor={setRgbColor} />
+    </>
   );
 }
 
---
+++ src/Components/Color.js
@@ -0,0 +1,47 @@
+import { useEffect } from 'react';
+import './my-style.css'
+
+function ColorFix({ color, setColor, rgbColor, setRgbColor }) {
+
+  const handleChange = evt => {
+    const value = evt.target.value;
+    if (value.length === 1 && value !== '#') {
+      evt.target.value = '#';
+    }
+    setColor(value);
+  }
+
+  function isValueValid(string) {
+    const regHex = /^#[0-9a-f]{1,6}$/i;
+    return regHex.test(string);
+  }
+
+  useEffect(() => {
+    if (color.length >= 7 && isValueValid(color)) {
+      let r = parseInt(color.slice(1, 3), 16);
+      let g = parseInt(color.slice(3, 5), 16);
+      let b = parseInt(color.slice(5, 7), 16);
+      const rgbColorValue = `rgb(${r},${g},${b})`;
+      setRgbColor(rgbColorValue)
+    }
+    else if (!isValueValid(color) && color.length >= 7) {
+      setRgbColor('Ошибка! Цвета не существует :(');
+    }
+  }, [color, setRgbColor]);
+
+  return (
+    <>
+      <div className="big-container" style={{ backgroundColor: `${color}` }}>
+        <div className="color-container">
+          <input className="color-container__input" onChange={handleChange} maxLength="7" />
+          <div className="color-container__value">
+            <p>{rgbColor}</p>
+          </div>
+        </div>
+      </div >
+    </>
+  )
+}
+
+export default ColorFix;
+
